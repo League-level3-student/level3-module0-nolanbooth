823c9ad9b74293ab15dd9d2cadb581f0afbbc18f
student <student@ip-172-31-52-136.us-west-2.compute.internal>
2022-12-17 10:58:58 -0800

    Work on intro to array lists thing

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -1,25 +1,31 @@
 package _00_Intro_To_Arrays;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+    	String[] strings = {"One", "Two", "Three", "Four", "Five"};
         // 2. print the third element in the array
-
+    	System.out.println(strings[2]);
         // 3. set the third element to a different value
-
+    	strings[2] = "THREEEEE";
         // 4. print the third element again
-
+    	System.out.println(strings[2]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
-
+    	for(int i = 0; i < strings.length; i++) {
+    		strings[i] = "doug demuro";
+    	}
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-
+    	for(int i = 0; i < strings.length; i++) {
+    		System.out.println(strings[i]);
+    	}
         // 7. make an array of 50 integers
-
+    	int[] numbers = {1,2,3,4};
+    	
         // 8. use a for loop to make every value of the integer array a random
         //    number
 
