f5645b31074fc6e744927c91013690551a4118f6
nolanbooth <56123783+nolanbooth@users.noreply.github.com>
2022-12-17 14:36:04 -0800

    Update _01_IntroToArrays.java

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -24,19 +24,38 @@ public class _01_IntroToArrays {
     		System.out.println(strings[i]);
     	}
         // 7. make an array of 50 integers
-    	int[] numbers = {1,2,3,4};
+    	int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50};
     	
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+        Random ran = new Random();
+        for(int i = 0; i < numbers.length; i++){
+         numbers[i] =  ran.nextInt(200);
+           
+        }
         // 9. without printing the entire array, print only the smallest number
         //    on the array
-
+        int smallestNumber = numbers[0];
+        for(int i = 0; i < numbers.length; i++){
+        if(numbers[i] < smallestNumber){
+            smallestNumber = numbers[i];
+            }
+        }
+        System.out.println(smallestNumber);
         // 10 print the entire array to see if step 8 was correct
-
+        for(int i = 0; i < numbers.length; i++){
+         System.out.println(numbers[i]);   
+        }
         // 11. print the largest number in the array.
-
+        int largestNumber = numbers[0];
+        for(int i = 0; i < numbers.length; i++){
+        if(numbers[i] > largestNumber){
+            largestNumber = numbers[i];
+            }
+        }
+        System.out.println(largestNumber);
+        
         // 12. print only the last element in the array
-
+        System.out.println(numbers[49]);
     }
 }
