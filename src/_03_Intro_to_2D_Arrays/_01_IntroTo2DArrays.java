a7855e0de101c606204d01f296e77fc0fa4db799
student <student@ip-172-31-48-45.us-west-2.compute.internal>
2022-12-18 10:23:14 -0800

    Got stuck on the 2d array thingies

--- src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
+++ src/_03_Intro_to_2D_Arrays/_01_IntroTo2DArrays.java
@@ -5,11 +5,16 @@ public class _01_IntroTo2DArrays {
     public static void main(String[] args) {
         // 1. declare a 2D array of integers with 3, 1D arrays that each hold
         //    3 integers
-
+    	int[][] numbers = new int[3][3];
         // 2. use two for loops to set all the elements in the 2D array to
         //    a different integer
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-        
+        for(int i = 0; i < numbers.length; i++) {
+        	for(int j = 0; j < numbers[i].length; j++) {
+        		numbers[i][j] = i;
+        		numbers[j][i] = j;
+        	}
+        }
         // 3. print the third element from the second 1D array (2DArray[1][2])
 
         // 4. set the third element from the second 1D array to a different
